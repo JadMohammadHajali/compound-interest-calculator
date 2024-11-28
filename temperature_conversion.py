23dad6ad2e5443b244bd6e0a0d989a9f51d57d31
JadMohammadHajali <jadhajali773@gmail.com>
2024-11-28 14:53:11 +0200

    Temperature Conversion Calculator Project

---
+++ temperature_conversion.py
@@ -0,0 +1,32 @@
+""" Temperature Conversion Calculator Project
+
+The program will display the user's new balance after accruing interest over the specified number of years.
+"""
+
+principle = 0
+time = 0
+rate = 0
+
+while True:
+    principle = float(input("Enter the principle amount: "))
+    if principle < 0:
+        print("Principle can't be less than or equal to zero.")
+    else:
+        break
+
+while True:
+    rate = float(input("Enter the interest rate: "))
+    if rate < 0:
+        print("Interest rate can't be less than or equal to zero.")
+    else:
+        break
+
+while True:
+    time = float(input("Enter the time in years: "))
+    if time <= 0:
+        print("The time can't be less than or equal to zero.")
+    else:
+        break
+
+total = principle * pow((1 + rate/100), time)
+print(f"Balance after {time} years/s: {total:.2f}")
